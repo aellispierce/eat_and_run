cbeb6eb5ad58facdf88ba62cc1adc7e94a77b547
Topstitch <kallen@spoonflower.com>
2015-03-06 16:38:23 -0500

    Take brand name out of search

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,7 +1,7 @@
 class Food
   attr_reader :food_info
   def initialize(food)
-    @food_info = HTTParty.get("https://api.nutritionix.com/v1_1/search/#{food}?results=0:20&fields=item_name,brand_name,item_id,nf_calories&appId=#{ENV['NUTRITIONIX_ID']}&appKey=#{ENV['NUTRITIONIX_TOKEN']}")
+    @food_info = HTTParty.get("https://api.nutritionix.com/v1_1/search/#{food}?results=0:20&fields=item_name,item_id,nf_calories&appId=#{ENV['NUTRITIONIX_ID']}&appKey=#{ENV['NUTRITIONIX_TOKEN']}")
   end
 
 end
