1f91c393c9fbac54b70006ffa133a7b1751e7c5a
Ashley Ellis <anellis12@gmail.com>
2015-03-05 21:36:26 -0500

    Edit tests

--- test/controllers/api/v1/trails_controller_test.rb
+++ test/controllers/api/v1/trails_controller_test.rb
@@ -12,16 +12,16 @@ module Api
         get :index
         assert_response :success
         result = JSON.parse(response.body)
-        result["trail"]["full_trail_list"]["places"].length
-        assert result.length > 0
+        num_results = result["trail"]["full_trail_list"]["places"].length
+        assert num_results > 0
       end
 
       test "index should have foods" do
         get :index
         assert_response :success
         result = JSON.parse(response.body)
-        result["food"]["food_info"]["hits"].length
-        assert result.length > 0
+        num_results= result["food"]["food_info"]["hits"].length
+        assert num_results > 0
       end
     end
   end
