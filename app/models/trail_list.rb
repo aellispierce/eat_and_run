53f87b74751c1763a656e84a85ccfde4c09f789f
Ashley Ellis <anellis12@gmail.com>
2015-03-06 20:04:33 -0500

    Refactor trails method to reduce duplication

--- app/models/trail_list.rb
+++ app/models/trail_list.rb
@@ -4,39 +4,19 @@ class TrailList
     @full_trail_list= HTTParty.get("https://outdoor-data-api.herokuapp.com/api.json?api_key=#{ENV['TRAIL_TOKEN']}&q[city_eq]=#{city}")
   end
 
-  def parse_length(activities)
-    length = activities.map do |a|
-       a["length"]
+  def parse(activities, attribute)
+    attribute = activities.map do |a|
+       a[attribute]
     end
-    length[0]
-  end
-  def parse_description(activities)
-    description = activities.map do |a|
-      a["description"]
-    end
-    description[0]
-  end
-
-  def parse_activity_type(activities)
-    type = activities.map do |a|
-      a["activity_type_name"].capitalize
-    end
-    type[0]
-  end
-
-  def parse_thumbnail(activities)
-    type = activities.map do |a|
-      a["thumbnail"]
-    end
-    type[0]
+    attribute[0]
   end
 
   def trails
     names = @full_trail_list["places"].map {|place| place["name"]}
-    lengths = @full_trail_list["places"].map {|place| parse_length(place["activities"])}
-    descriptions = @full_trail_list["places"].map {|place| parse_description(place["activities"])}
-    activity_types = @full_trail_list["places"].map {|place| parse_activity_type(place["activities"])}
-    thumbnails = @full_trail_list["places"].map {|place| parse_thumbnail(place["activities"])}
+    lengths = @full_trail_list["places"].map {|place| parse(place["activities"], "length")}
+    descriptions = @full_trail_list["places"].map {|place| parse(place["activities"], "description")}
+    activity_types = @full_trail_list["places"].map {|place| parse(place["activities"], "activity_type_name")}
+    thumbnails = @full_trail_list["places"].map {|place| parse(place["activities"], "thumbnail")}
 
     trails= []
     names[0..4].each_with_index do |name, index|
