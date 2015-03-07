cc70377a2e09159a59c4a6f762d51c396822d5de
Topstitch <kallen@spoonflower.com>
2015-03-07 13:19:07 -0500

    Fix bug with nil length

--- app/models/trail_list.rb
+++ app/models/trail_list.rb
@@ -36,8 +36,7 @@ class TrailList
   def closest_match_trails(target_miles)
     low_end = target_miles - 3
     high_end = target_miles + 3
-    # @lengths
-    results = @lengths[0..4].select {|length|  length >= low_end && length <= high_end}
+    results = @lengths[0..-1].select {|length| length != nil && length >= low_end && length <= high_end}
     if results.count >= 1
       results
     else
@@ -65,34 +64,8 @@ class TrailList
 
   def as_json(options = {})
 
-    {trails: included_trails, miles_needed: target_miles, matching_trails: closest_match_trails(target_miles)}
+    {miles_needed: target_miles, trails: included_trails}
 
   end
 
 end
-# def trails
-#   names = @full_trail_list["places"].map {|place| place["name"]}
-#   @lengths = @full_trail_list["places"].map {|place| parse_length(place["activities"])}
-#   descriptions = @full_trail_list["places"].map {|place| parse_description(place["activities"])}
-#   activity_types = @full_trail_list["places"].map {|place| parse_activity_type(place["activities"])}
-#   thumbnails = @full_trail_list["places"].map {|place| parse_thumbnail(place["activities"])}
-#
-#   trails= []
-#   lengths[0..4].each_with_index do |length, index|
-#     if length > 5
-#       trails << "Name: #{names[index]}  \n Length: #{length} \n Description: #{descriptions[index]} \n" +
-#       "Activity Type: #{activity_types[index]}\n Picture: #{thumbnails[index]}\n"
-#     end
-#   end
-#   trails
-# end
-# def included_lengths
-#   matches= closest_match_trails(target_miles)
-#   @lengths.each_with_index do |length, index|
-#     if matches.include?(length)
-#       included_trail_indices << index
-#     end
-#     included_trail_indices.each do |index|
-#       name[index] length[index] description[index]
-#     end
-#   end
