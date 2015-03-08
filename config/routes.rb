13134e955fe66f6149a38b02c31190543cc5cca2
Ashley Ellis <anellis12@gmail.com>
2015-03-08 19:23:16 -0400

    Add home controller with index action as root page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
+  get 'home/index'
+
   namespace :api do
     namespace :v1 do
       resources :trails
+      root 'trails#index'
     end
   end
+
+  root 'home#index'
 end
